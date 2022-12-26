a970bc5840a456d3f2436012e3a0a6211bf5560e
Varuni P <varunipherle@gmail.com>
2022-12-26 09:37:14 +0530

    changes

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import Home from './Homepage/Home';
 import Peace from './Pages/Peace';
 import Random from './Pages/Random';
@@ -11,13 +11,13 @@ function Router() {
   return (
     <div>
         
-        <BrowserRouter>
+        <HashRouter>
         <Routes>
             <Route path = "random" element={<Random />}></Route>
             <Route path = "peace" element={<Peace />}></Route>
             <Route path = "/" element = {<Home />}></Route>
         </Routes>
-        </BrowserRouter>
+        </HashRouter>
     </div>
 
   )
